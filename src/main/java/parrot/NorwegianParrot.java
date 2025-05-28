b58a579a42706456f5f41f8ec52c3049663ea5d3
franciscocamellon <franciscocamellon@gmail.com>
2025-05-28 17:10:03 -0300

    Refactoring - remove redundant code in the all subclasses

--- src/main/java/parrot/NorwegianParrot.java
+++ src/main/java/parrot/NorwegianParrot.java
@@ -1,16 +1,14 @@
 package parrot;
 
+import static parrot.ParrotTypeEnum.NORWEGIAN_BLUE;
+
 public class NorwegianParrot extends Parrot {
     public NorwegianParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(ParrotTypeEnum.NORWEGIAN_BLUE, numberOfCoconuts, voltage, isNailed);
+        super(NORWEGIAN_BLUE, numberOfCoconuts, voltage, isNailed);
     }
 
     @Override
     public double getSpeed() {
-        return switch (type) {
-            case EUROPEAN -> getBaseSpeed();
-            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
-        };
+        return (isNailed) ? 0 : getBaseSpeed(voltage);
     }
 }
